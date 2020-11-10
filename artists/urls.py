5dc26f30e630bb418d51e3401e7d83f601c41df1
chastinet <victorch@petco.com>
2020-11-10 17:13:48 -0300

    adding new filters to viewset

--- artists/urls.py
+++ artists/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from artists.viewsets import ArtistsViewSet, TagsViewSet, StatesViewSet
@@ -10,7 +10,5 @@ router.register(r'states', StatesViewSet)
 urlpatterns = [
   path('', include(router.urls)),
   path('artists/', ArtistsViewSet.as_view({'get': 'list'})),
-  path('artist/<int:pk>/', ArtistsViewSet.as_view({'get': 'retrieve'})),
-  path('artists-by-tag/<int:tag_id>', ArtistsViewSet.as_view({'get': 'artist_by_tag'})),
-  path('artists-by-state/<str:uf>', ArtistsViewSet.as_view({'get': 'artist_by_state'})),
+  path('artist/<int:id>/', ArtistsViewSet.as_view({'get': 'retrieve'})),
 ]
--- artists/viewsets.py
+++ artists/viewsets.py
@@ -1,26 +1,48 @@
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from artists.serializers import ArtistSerializer, StateSerializer, TagSerializer
 from . import models
 
 
 class ArtistsViewSet(viewsets.ModelViewSet):
-  
   queryset = models.Artist.objects.all()
   serializer_class = ArtistSerializer
 
-  def artist_by_tag(self, request, tag_id):
-
-    queryset = self.get_queryset.filter(tags=tag_id)
+  def list(self, request, *args, **kwargs):
+    tag_params = request.GET.get('tags')
+    name_params = request.GET.get('name')
+    uf_params = request.GET.get('uf')
+    
+    if tag_params:
+      return self.artist_by_tag(request, tag_params)
+    elif name_params:
+      return self.artist_by_name(request, name_params)
+    elif uf_params:
+      return self.artist_by_state(request, uf_params)
+    else:
+      return super().list(request, *args, **kwargs)
+  
+  def artist_by_tag(self, request, tag_params):
+    tags = tag_params.split(',')
+    queryset = self.get_queryset().filter(tags__in=tags).annotate(num_tags=Count('tags')).filter(num_tags__gte=len(tags))
     page = self.paginate_queryset(queryset)
     serialize = self.get_serializer(page, many=True)
 
     return self.get_paginated_response(serialize.data)
 
-  def artist_by_state(self, request, uf):
+  def artist_by_state(self, request, uf_params):
+    queryset = self.get_queryset().filter(state__uf=uf_params.upper())
+    page = self.paginate_queryset(queryset)
+    serialize = self.get_serializer(page, many=True)
 
-    queryset = self.get_queryset.filter(state__uf=uf.upper())
+    return self.get_paginated_response(serialize.data)
+
+    
+  def artist_by_name(self, request, name_params):
+    queryset = self.get_queryset().filter(name__icontains=name_params)
     page = self.paginate_queryset(queryset)
     serialize = self.get_serializer(page, many=True)
 
