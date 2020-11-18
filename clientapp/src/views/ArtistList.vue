49e60b8c126e72e44a67750764dfba55ae06c555
chastinet <victorch@petco.com>
2020-11-18 17:37:20 -0300

    calling artists endpoint

--- clientapp/src/views/ArtistList.vue
+++ clientapp/src/views/ArtistList.vue
@@ -18,12 +18,12 @@
         :key="item.title"
       >
         <v-list-item-avatar>
-          <v-img :src="item.avatar"></v-img>
+          <v-img :src="item.portfolio"></v-img>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title v-html="item.title"></v-list-item-title>
-          <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-list-item-title v-html="item.name"></v-list-item-title>
+          <v-list-item-subtitle v-html="item.state"></v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </template>
@@ -32,44 +32,44 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+interface ServerResponse {
+  results,
+  count,
+  next,
+  previous,
+}
+
+interface Artist {
+    name: string,
+    id: number,
+    contact: string,
+    portfolio: string,
+    state: string,
+    instagram_username: string,
+    tags: Tag[],
+}
+
+interface Tag {
+  id: number,
+  name: string,
+  description: string,
+}
+
 
 @Component({
   components: {
     // StartScreen
   },
   data: () => ({
-      items: [
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Birthday gift',
-          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
+    items: [] as Artist[],
     }),
+    mounted: async function() {
+      const { data } = await axios.get('http://localhost:8000/api/artists/')
+      this.items = data.results;
+
+    }
 })
 export default class ArtistList extends Vue {}
 </script>
